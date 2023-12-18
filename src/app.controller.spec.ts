5c2407bed17e325980ad4da1c0eff31c82b9aa75
Rizki Budi Prasetyo <rizkibudi513@gmail.com>
2023-12-18 18:08:02 +0700

    feat: update absolute path

--- src/app.controller.spec.ts
+++ src/app.controller.spec.ts
@@ -1,4 +1,5 @@
 import { Test, TestingModule } from '@nestjs/testing';
+
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
@@ -18,102 +19,15 @@ describe('AppController', () => {
     it('should return "Hello World!"', () => {
       expect(appController.getHello()).toMatchInlineSnapshot(`
         {
-          "data": "Hello World!",
-          "response": {
-            "code": 200,
-            "message": "OK",
-          },
+          "message": "Hello World!",
         }
       `);
     });
 
-    it('should return http status', () => {
-      expect(appController.getHttpStatus({})).toMatchInlineSnapshot(`
-        {
-          "data": {
-            "errorCode": 200,
-            "message": "OK",
-          },
-          "response": {
-            "code": 200,
-            "message": "OK",
-          },
-        }
-      `);
-
-      expect(appController.getHttpStatus({ code: 400 })).toMatchInlineSnapshot(`
-        {
-          "data": {
-            "errorCode": 400,
-            "message": "Bad Request",
-          },
-          "response": {
-            "code": 400,
-            "message": "Bad Request",
-          },
-        }
-      `);
-
-      expect(appController.getHttpStatus({ code: 400, error_code: 1001 }))
-        .toMatchInlineSnapshot(`
-        {
-          "data": {
-            "errorCode": 1001,
-            "message": "Bad Request",
-          },
-          "response": {
-            "code": 400,
-            "message": "Bad Request",
-          },
-        }
-      `);
-
-      expect(
-        appController.getHttpStatus({
-          code: 400,
-          error_code: 1001,
-          message: 'Custom message',
-        }),
-      ).toMatchInlineSnapshot(`
-        {
-          "data": {
-            "errorCode": 1001,
-            "message": "Custom message",
-          },
-          "response": {
-            "code": 400,
-            "message": "Bad Request",
-          },
-        }
-      `);
-
-      expect(appController.getHttpStatus({ code: 1000 }))
-        .toMatchInlineSnapshot(`
-        {
-          "data": {
-            "errorCode": 1000,
-            "message": "Bad Request",
-          },
-          "response": {
-            "code": 400,
-            "message": "Bad Request",
-          },
-        }
-      `);
-
-      expect(appController.getHttpStatus({ code: 1000, error_code: 1001 }))
-        .toMatchInlineSnapshot(`
-        {
-          "data": {
-            "errorCode": 1001,
-            "message": "Bad Request",
-          },
-          "response": {
-            "code": 400,
-            "message": "Bad Request",
-          },
-        }
-      `);
+    it('should throw an error', () => {
+      expect(() =>
+        appController.getHello({ error: 'true' }),
+      ).toThrowErrorMatchingInlineSnapshot(`"General Exception"`);
     });
   });
 });
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,7 @@
-import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
+import { Module } from '@nestjs/common';
+
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { ResponseInterceptor } from './common/interceptors';
 
 @Module({
   imports: [],
